09b59d2a184c818519225ae545abeddf60f0014c
vcolon24 <valeria2005lps@gmail.com>
2024-09-18 17:11:13 -0400

    added temp code

--- Assignment1.java
+++ Assignment1.java
@@ -1,7 +1,35 @@
-// Import libraries here
 
 public class Assignment1 {
   public static void main(String[] args) {
-    // Add your code here
+    System.out.println("VV      VV CCCCCCCC");
+    System.out.println(" VV    VV  CC      ");
+    System.out.println("  VV  VV   CC      ");
+    System.out.println("   VVVV    CC      ");
+    System.out.println("    VV     CCCCCCCC");
+
+    System.out.print("please enter a tempeture in fahrenheit: \n");
+    final int BASE = 32;
+    final double CONVERSION_FACTOR = 9.0 / 5.0;
+
+    double fahrenheitTemp;
+    int celsiusTemp = 0;  // value to convert
+
+    fahrenheitTemp = celsiusTemp * CONVERSION_FACTOR + BASE;
+
+    System.out.println(+ fahrenheitTemp);
+    System.out.print("please enter a 5-string character: \n");
+    String originalstring = "vahlc";
+    StringBuilder sb = new StringBuilder(originalstring);
+    String reversedString = sb.reverse().toString();
+    System.out.println(reversedString);
+
+    System.out.print("Random number generated. continuing... \n");
+    Funciton getRandomNumber
+
+    reversedString.substring(1,4);
+    StringBuilder sb = new StringBuilder(reversedString)
+    System.out.println("Your new string is" + (reversedString);
+
   }
-}
+
+  }
